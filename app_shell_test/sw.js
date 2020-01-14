57dac60b68670c71aace4c8a9506740fba66f4e3
yangshang01 <yangshang01@58.com>
2020-01-14 17:16:12 +0800

    feat: update

--- app_shell_test/sw.js
+++ app_shell_test/sw.js
@@ -19,15 +19,68 @@ self.addEventListener('install', function(e) {
 
 
 // 捕获请求并返回缓存数据
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(caches.match(event.request).catch(function() {
+//     return fetch(event.request);
+//   }).then(function(response) {
+//     caches.open(cacheName).then(function(cache) {
+//       cache.put(event.request, response);
+//     });
+//     return response.clone();
+//   }).catch(function() {
+//     return caches.match('./img/logo.png');
+//   }));
+// });
+
+
+
+// 捕获请求并返回缓存数据
 self.addEventListener('fetch', function(event) {
-  event.respondWith(caches.match(event.request).catch(function() {
-    return fetch(event.request);
-  }).then(function(response) {
-    caches.open(cacheName).then(function(cache) {
-      cache.put(event.request, response);
-    });
-    return response.clone();
-  }).catch(function() {
-    return caches.match('./img/logo.png');
-  }));
+  event.respondWith(
+    caches.open(cacheName).then(cache => {
+      // console.log(">>>>>request", event.request);
+
+      return cache.match(event.request).then(res => {
+        if (res) {
+          console.log('in cache', res);
+          return res;
+        }
+
+        return fetch(event.request).then(netRes => {
+          cache.put(event.request, netRes.clone());
+          return netRes;
+        });
+      }).catch(err => {
+        console.log(err);
+        throw err;
+      });
+    })
+  );
+});
+
+
+// 安装阶段跳过等待，直接进入 active
+self.addEventListener('install', function (event) {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+      Promise.all([
+
+          // 更新客户端
+          self.clients.claim(),
+
+          // 清理旧版本
+          caches.keys().then(function (cacheList) {
+              return Promise.all(
+                  cacheList.map(function (cacheName) {
+                      if (cacheName !== 'my-test-cache-v1') {
+                          return caches.delete(cacheName);
+                      }
+                  })
+              );
+          })
+      ])
+  );
 });
